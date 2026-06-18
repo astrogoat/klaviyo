98d8ac8b97f4756766af1d1b376e91d0fa270401
OniiCode <oniicoder@gmail.com>
2026-06-18 16:15:08 +0000

    Add more configs

--- database/migrations/settings/2026_05_26_154408_create_klaviyo_settings.php
+++ database/migrations/settings/2026_05_26_154408_create_klaviyo_settings.php
@@ -7,12 +7,16 @@ return new class extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('klaviyo.enabled', false);
+        $this->migrator->add('klaviyo.private_api_key', '');
         $this->migrator->add('klaviyo.company_id', '');
+        $this->migrator->add('klaviyo.form_id', '');
     }
 
     public function down()
     {
         $this->migrator->delete('klaviyo.enabled');
+        $this->migrator->delete('klaviyo.private_api_key');
         $this->migrator->delete('klaviyo.company_id');
+        $this->migrator->delete('klaviyo.form_id');
     }
 };
